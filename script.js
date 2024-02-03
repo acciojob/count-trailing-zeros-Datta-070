2c58a37abcf29488d3a329c052878b59f65bab7b
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-02-03 15:40:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let result=0;
+	while(n >= 5){
+		n=Math.floor(n/5);
+		result += n;
+	}
+	return result;
 }
 
 const input = prompt("Enter a number");
