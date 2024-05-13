63e19b99fa97dfeeb62b6afbaf6aadf9049a8906
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-05-13 09:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function trailingZeros(n) {
+function factorial(n) {
   //your JS code here. If required.
-	let result=0;
-	while(n >= 5){
-		n=Math.floor(n/5);
-		result += n;
-	}
-	return result;
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
+
+function trailingZeros(input) {
+    let factorialResult = factorial(input);
+    let count = 0;
+    
+    while (factorialResult % 10 === 0) {
+        count++;
+        factorialResult = factorialResult / 10;
+    }
+    
+    return count;
+}
+
 
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
